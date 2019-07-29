a38e09e34271f322cba766b79fd8f9945fac668a
Ayman <aymalsh@gmail.com>
2019-07-29 06:37:13 +0300

    Add App Widget that displays the favorite recipe

---
+++ app/src/main/java/com/aymanshehri/bakingapp/MyAppWidgetProvider.java
@@ -0,0 +1,46 @@
+package com.aymanshehri.bakingapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MyAppWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("com.aymanshehri.bakingapp.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
+
+        CharSequence widgetTitle = sharedPreferences.getString("widget_title", "");
+        CharSequence widgetContent = sharedPreferences.getString("widget_ingredients", "");
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget);
+        views.setTextViewText(R.id.recipe_title, widgetTitle);
+        views.setTextViewText(R.id.recipe_ingredients, widgetContent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
